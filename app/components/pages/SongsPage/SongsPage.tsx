1cf34dcbebb790da81a1864c4e19a555c228ae33
Akos Sereg <akos.sereg@gmail.com>
2026-01-20 11:33:03 +0100

    Adding more sheets and midi

--- app/components/pages/SongsPage/SongsPage.tsx
+++ app/components/pages/SongsPage/SongsPage.tsx
@@ -9,6 +9,7 @@ interface Song {
   file: string;
   coverImage: string;
   type: 'full' | 'riff';
+  format: 'json' | 'midi';
 }
 
 const Songs: Song[] = [
@@ -16,91 +17,134 @@ const Songs: Song[] = [
     title: 'Jurassic Park',
     file: 'jurassic-park.json',
     coverImage: 'jurassic-park.png',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'Peppa Pig',
     file: 'peppa-pig.json',
     coverImage: 'peppa-pig.jpg',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Mad World',
     file: 'mad-world.json',
     coverImage: 'mad-world.jpg',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'Through My Words',
     file: 'through-my-words.json',
     coverImage: 'scenes-from-a-memory.jpg',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
+  {
+    title: 'Fatal Tragedy',
+    file: 'fatal-tragedy.json',
+    coverImage: 'scenes-from-a-memory.jpg',
+    type: 'full',
+    format: 'json'
+},
   {
     title: 'Star Wars (basic)',
     file: 'star-wars.json',
     coverImage: 'star-wars.png',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Star Wars',
     file: 'star-wars-advanced.json',
     coverImage: 'star-wars.png',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Clocks',
     file: 'clocks.json',
     coverImage: 'clocks.png',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Pyramid Song',
     file: 'pyramid-song.json',
     coverImage: 'pyramid-song.png',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Teardrop',
     file: 'teardrop.json',
     coverImage: 'mezannine.jpg',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'Nobody Home',
     file: 'nobody-home.json',
     coverImage: 'the-wall.jpg',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'The great gig in the sky',
     file: 'the-great-gig-in-the-sky.json',
     coverImage: 'dark-side-of-the-moon.jpg',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'The Scientist',
     file: 'the-scientist.json',
     coverImage: 'the-scientist.png',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'Bohemian Rhapsody',
     file: 'bohemian-rhapsody.json',
     coverImage: 'freddy.png',
-    type: 'riff'
+    type: 'riff',
+    format: 'json'
   },
   {
     title: 'Imagine',
     file: 'imagine.json',
     coverImage: 'imagine.png',
-    type: 'full'
+    type: 'full',
+    format: 'json'
   },
   {
     title: 'Inception',
     file: 'inception.json',
     coverImage: 'inception.png',
-    type: 'full'
+    type: 'full',
+    format: 'json'
+  },
+  {
+    title: 'Is There Life On Mars',
+    file: 'is-there-life-on-mars.mid',
+    coverImage: 'is-there-life-on-mars.png',
+    type: 'riff',
+    format: 'midi'
+  },
+  {
+    title: 'November Rain',
+    file: 'november-rain.mid',
+    coverImage: 'november-rain.png',
+    type: 'riff',
+    format: 'midi'
+  },
+  {
+    title: 'The Entertainer',
+    file: 'the-entertainer.mid',
+    coverImage: 'the-entertainer.png',
+    type: 'riff',
+    format: 'midi'
   }
 ];
 
@@ -111,12 +155,17 @@ const SongsPage = () => {
   const handleSelectSong = useCallback(async (e: any, song: Song) => {
     e.preventDefault();
 
+    if (song.format === 'midi') {
+      document.location.href = `/static/midi/${song.file}`;
+      return
+    }
+
+
     const response = await fetch(`/static/sheets/${song.type}/${song.file}`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    // const json = await response.json();
     localStorage.setItem('most-recent-data', await response.text());
     setPlaying(false);
     setPlayingEpisode(0);
